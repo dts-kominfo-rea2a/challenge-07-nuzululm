06455425461b5fbbadffb09a8392464b63e1f782
nuzululm <nuzul.sealovers@gmail.com>
2022-06-12 20:28:04 +0700

    feat: menambahkan challenge 07 Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const IIX = await promiseTheaterIXX();
+    const VGC = await promiseTheaterVGC();
+
+    const unionData = IIX.concat(VGC);
+
+    let result = unionData.filter(
+      element => (element.hasil === emosi)
+    )
+
+    return result.length;
+
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
